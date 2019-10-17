3f7288920c46aa94f26a81a0c3ce2723946354dd
Muhammad Hussain <hussain@bigdataanalytics.pk>
2019-10-17 15:53:35 +0500

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -8,15 +8,14 @@ import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
 
 const jsx = (
  <Provider store={store}>
     <AppRouter />
  </Provider>
 );
+
 ReactDOM.render(jsx, document.getElementById('app'));
